71cb325260b33349cd4a4e38da7eb006b2dde870
Justin Shaw <easyworkemail@gmail.com>
2019-04-27 17:54:51 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,7 +15,7 @@ def valid_move?(board, index)
   if index.between?(0,8) || board[index] == "" || board[index] == " "
     return true
   else
-    return false
+    return turn(board)
   end
 end
 
