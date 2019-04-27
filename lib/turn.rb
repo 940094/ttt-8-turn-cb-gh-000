c325eebd95441eecbfec2c515d583f63472709f0
Justin Shaw <easyworkemail@gmail.com>
2019-04-27 17:46:30 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,7 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-def input_to_index(input)
+def input_to_index(input)  # converts a user_input to an integer -1
   return input.to_i -= 1
 end
 
