92b1ed23c79e26c7631260bfda5b7bb31233efb9
Justin Shaw <easyworkemail@gmail.com>
2019-04-27 17:41:29 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -6,6 +6,10 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(index)
+  return index = index - 1
+end
+
 def valid_move?(board, index)
   if index.between?(0,8) || board[index] == "" || board[index] == " "
     return true
